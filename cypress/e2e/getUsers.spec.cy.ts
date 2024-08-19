f4249e02161e609dbf82866a355a432abfe2f96b
Victor Prado <vmpradob@gmail.com>
2024-08-18 20:47:50 -0400

    feat: add GET /users integration tests

--- cypress/e2e/getUsers.spec.cy.ts
+++ cypress/e2e/getUsers.spec.cy.ts
@@ -14,9 +14,9 @@ describe(`Users API Integration Tests`, () => {
       method: 'get',
     }).then((res) => {
       expect(res.status).eq(200);
-      expect(res.body.page).to.eq('1');
-      expect(res.body.limit).to.eq('20');
-      expect(res.body.sort).to.eq('-1');
+      expect(res.body.page).to.eq(1);
+      expect(res.body.limit).to.eq(20);
+      expect(res.body.sort).to.eq(-1);
       expect(res.body.sortBy).to.eq('createdAt');
       expect(res.body.data.length).to.be.greaterThan(0);
       expect(res.body.data.every((user) => user.firstName.match(/john/i)));
@@ -29,9 +29,9 @@ describe(`Users API Integration Tests`, () => {
       method: 'get',
     }).then((res) => {
       expect(res.status).eq(200);
-      expect(res.body.page).to.eq('2');
-      expect(res.body.limit).to.eq('20');
-      expect(res.body.sort).to.eq('-1');
+      expect(res.body.page).to.eq(2);
+      expect(res.body.limit).to.eq(20);
+      expect(res.body.sort).to.eq(-1);
       expect(res.body.sortBy).to.eq('firstname');
       expect(res.body.data.length).to.be.eq(20);
     });
@@ -43,9 +43,9 @@ describe(`Users API Integration Tests`, () => {
       method: 'get',
     }).then((res) => {
       expect(res.status).eq(200);
-      expect(res.body.page).to.eq('1');
-      expect(res.body.limit).to.eq('1');
-      expect(res.body.sort).to.eq('-1');
+      expect(res.body.page).to.eq(1);
+      expect(res.body.limit).to.eq(20);
+      expect(res.body.sort).to.eq(-1);
       expect(res.body.sortBy).to.eq('firstname');
       expect(res.body.data.length).to.be.eq(20);
     });
