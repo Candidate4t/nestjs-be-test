8a4bc6d1b928565d25ecd8070acfa444a7ac6390
Victor Prado <vmpradob@gmail.com>
2024-08-18 20:43:08 -0400

    fix: add transformation of request params to datatypes defined in DTO objects

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { IAppConfig } from 'config/app.config';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   const configService = app.get(ConfigService);
   const appConfig = configService.get<IAppConfig>('appConfig', { infer: true });
   const swaggerPath = appConfig.apiDocsPath;
